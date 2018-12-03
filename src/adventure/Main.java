c32db94d8edd97e2b287e7fe9217a68de2c4fbba
clare k <kirabo646@googlemail.com>
2018-12-03 15:04:27 +0000

    added more scenarios that will be randomly generated, need to fix the user input as entering the wrong prameters sometimes it skips certain steps.

--- src/adventure/Main.java
+++ src/adventure/Main.java
@@ -10,33 +10,60 @@ public class Main {
 	
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your Name : ");
+		
 		String name = sc.nextLine();
 		Player Player1 = new Player(name);	
 		System.out.println("Player Name: " + Player1.getName());
 		
-		Scenarios script = new Scenarios();
-	
-		System.out.println(script.startMission());
-		
-		String command = sc.nextLine();
-		System.out.println(script.prelude(command));
-		
-		
-		
-		
-		
-		
-	//	Compass MagicCompas = new Compass();	
-	//	System.out.println(MagicCompas.showDistance());
-		
-		
-	//	GridMap compassGrid = new GridMap();
-	//	compassGrid.createMap();
-		
-		
+		runScript(sc);
 		
 		
 		
 	}
+	
+	public static void runScript(Scanner sc) {
+		Scenarios script = new Scenarios();
+		System.out.println(script.startMission());
 
+		
+		String command = sc.nextLine();
+		
+		if(command.contentEquals("look")) {
+		System.out.println(script.enterLook(command));
+		}
+		else {
+			System.out.println(script.enterLook(command));
+			
+		}
+		
+		
+		
+		String direction = sc.nextLine();
+		System.out.println(script.chooseDirection(direction));
+		
+	
+	
+		while(direction != null) {
+			String newDirection = sc.nextLine();
+			//return distance??
+				 
+			if(direction.contentEquals("exit")) {
+				break;
+			} 
+			else {
+				System.out.println(script.chooseDirection(newDirection));
+			}
+			
+	       	
+		}
+		
+		
+	}
+	
+	
+	
 }
+	
+
+
+
--- src/adventure/Scenarios.java
+++ src/adventure/Scenarios.java
@@ -1,5 +1,8 @@
 package adventure;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Scenarios {
 
 	
@@ -9,17 +12,88 @@ public class Scenarios {
 		return firstText;
 	}
 	
-	public String partTwo() {
-		String secondLine = "Unknown to what lies in either direction you decide your fate";
-		return secondLine;
-	}
-	
-	public String prelude(String look) {
-	
+	public String enterLook(String look) {
+		
 		if ( look.contentEquals("look")) {
 			return partTwo().toString();
 		}
 		return "Please return the word Look";
 	}
+	
+	public String partTwo() {
+		String secondLine = "Unknown to what lies in either direction you decide your fate.";
+		String compasssDialogue = "You notice a small watch-like object in your pocket, it "
+			                    	+ " appears to be a compass.";
+		String direction = "Choose either : North , East , South , West";
+		return  secondLine + "\n" + 
+	        	compasssDialogue + "\n" 
+		        + direction;
+	}
+	
+    
+
+	public String chooseDirection(String direction) {
+		
+		if (direction.contentEquals("north")) {
+	
+			return randomObjectGenerator();
+			
+		}
+		else if (direction.contentEquals("east")) {
+		
+			return randomObjectGenerator();
+			
+		}
+		else if (direction.contentEquals("south")) {
+
+			return randomObjectGenerator();
+			
+		}
+		else if (direction.contentEquals("west")) {
+
+			return randomObjectGenerator();
+			
+		}
+		else if (direction.contentEquals("exit")) {
+
+			return "Game Over";
+			
+		}
+		
+		
+		return " ";
+		
+		
+	}
+	
+	 public String randomObjectGenerator() {
+			
+			String tree = "You have approached a tree, great! theres nothing here...";
+			String apple = "Congratulations! You've found an apple. You throw it away because its rotten";
+			String box =  "You discover a box, its filled with dirt";
+			String boat = "You discover a boat! With this you escape the swamp. You Win! The End." + 
+			               "\n" + 
+					       "Enter exit to leave the game";
+			String nothing = "An eery fog thickens";
+			
+			ArrayList<String> pointsOfInterest = new ArrayList<String>(); 
+			pointsOfInterest.add(tree);
+			pointsOfInterest.add(apple);
+			pointsOfInterest.add(box);
+			pointsOfInterest.add(boat);
+			pointsOfInterest.add(nothing);
+			
+			
+			for(int i = 0; i < pointsOfInterest.size(); i++) {		
+				int r = new Random().nextInt(pointsOfInterest.size());
+				return pointsOfInterest.get(r);
+			}
+			
+			return "A dense fog appears";
+		}
+		
+		
+	
+	
 	
 }
